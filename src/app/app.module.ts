41b821cf9f38e81dd83d0b415a6036588be06ca6
vanan07 <Vivek.Ananth@albertsons.com>
2023-03-31 16:56:05 +0530

    Revert "Revert "created initial skeleton""
    This reverts commit 4b327bcbf2c9a8fc371682b208b2657a161a65ff.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
