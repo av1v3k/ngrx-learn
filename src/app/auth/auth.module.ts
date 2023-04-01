9e24ca6375786c79a01c0f6fc43b9587c8cc97ac
vanan07 <Vivek.Ananth@albertsons.com>
2023-04-01 16:52:00 +0530

    added prettier

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,16 +1,10 @@
-
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { AuthRoutingModule } from './auth-routing.module';
-import { RegisterComponent } from './components/register/register.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { AuthRoutingModule } from "./auth-routing.module";
+import { RegisterComponent } from "./components/register/register.component";
 
 @NgModule({
-    declarations: [
-        RegisterComponent
-    ],
-    imports: [
-        CommonModule,
-        AuthRoutingModule
-    ],
+  declarations: [RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
